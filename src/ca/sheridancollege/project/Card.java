3828a932e932004d4286b72731e877564103b7fc
sandh510 <sandh510@sheridancollege.ca>
2024-02-18 23:38:40 -0500

    initial commit

---
+++ src/ca/sheridancollege/project/Card.java
@@ -0,0 +1,34 @@
+/**
+ * SYST 17796 Project Base code.
+ * Students can modify and extend to implement their game.
+ * Add your name as an author and the date!
+ */
+package ca.sheridancollege.project;
+
+
+/**
+ * A class to represent a playing card.
+ */
+public class Card {
+
+    private final String suit;
+    private final String rank;
+
+    public Card(String suit, String rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+}
